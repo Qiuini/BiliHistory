e595f10f3d604516f66e8f4f22754a3b47a5ff66
mikeqiu95 <3184099936@qq.com>
2026-07-29 11:20:54 +0800

    ci: 修复 Linux artifact 通配，确保 .deb 上传
    upload-artifact 通配需匹配 bilihistory_<ver>_<arch>.deb 的下划线前缀

--- src/version.py
+++ src/version.py
@@ -7,8 +7,8 @@
 
 APP_NAME = "BiliHistory"
 APP_NAME_CN = "BiliHistory - B站历史记录管理工具"
-APP_VERSION = "1.0.11"
-APP_VERSION_TUPLE = (1, 0, 11)
+APP_VERSION = "1.0.12"
+APP_VERSION_TUPLE = (1, 0, 12)
 PUBLISHER = "Qiuini"
 APP_URL = "https://github.com/Qiuini/BiliHistory"
 
